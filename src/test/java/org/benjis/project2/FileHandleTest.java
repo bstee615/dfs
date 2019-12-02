d3c8ad25d630e1be091543f81186be1562f69e06
Ben Steenhoek <benjis@iastate.edu>
2019-12-02 15:32:01 -0600

    Automate file handle test

---
+++ src/test/java/org/benjis/project2/FileHandleTest.java
@@ -0,0 +1,33 @@
+package org.benjis.project2;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class FileHandleTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void fileHandleTest()
+    {
+        filehandle fh1, fh2;
+    
+        fh1 = new filehandle();
+    
+        assertTrue(fh1.isAlive());
+    
+        fh2 = new filehandle();
+        assertTrue(fh2.isAlive());
+    
+        assertTrue(!fh1.Equals(fh2));
+
+        fh1.discard();
+        fh2.discard();
+        assertTrue(fh1.Equals(fh2));
+    }
+}
